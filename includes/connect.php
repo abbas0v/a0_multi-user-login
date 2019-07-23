8ff098271f03a58cbe254efa4b54678ea5f9b63b
Musa Abbasov <contact@abbas0v.com>
2019-07-23 15:36:55 +0400

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,7 +1,7 @@
 <?php
 $dbhost = "localhost"; //Host
 $dbuser = "abbas0v"; //Database user
-$dbpass = "musaxeqani1990"; //Database password
+$dbpass = "123"; //Database password
 $dbname = "phpmultiuserlogin"; //Database name
 $conn = mysqli_connect("$dbhost", "$dbuser", "$dbpass", "$dbname"); //Connection
 mysqli_set_charset($conn,"utf8"); //UTF-8 for Turkish letters
