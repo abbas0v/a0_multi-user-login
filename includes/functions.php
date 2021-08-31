b0424a5bbc42a78bde4ec983ea084c4bf09273fd
Musa Abbasov <contact@abbas0v.com>
2021-08-31 20:56:16 +0400

    #1 fixed

--- includes/functions.php
+++ includes/functions.php
@@ -1,37 +1,38 @@
 <?php
-class Login{
-  public function LoginSystem(){
+class Login {
+  public function LoginSystem() {
     session_start(); // Starting Session
     $error = ''; // Variable To Store Error Message
-    if (isset($_POST['submit'])) {
+    if (!isset($_POST['submit'])) {
       if (empty($_POST['login']) || empty($_POST['password'])) {
         $error = "Username or Password is invalid";
       }
-      else{
-        include 'connect.php';
-        // Define $username and $password
-        $username = $_POST['login'];
-        $password = md5($_POST['password']);
-        // SQL query to fetch information of registerd users and finds user match.
-        $query = "SELECT login, password FROM users WHERE login=? AND password=? LIMIT 1";
-        // To protect MySQL injection for Security purpose
-        $stmt = $conn->prepare($query);
-        $stmt->bind_param("ss", $username, $password);
-        $stmt->execute();
-        $stmt->bind_result($username, $password);
-        $stmt->store_result();
-        if($stmt->fetch()) { //fetching the contents of the row 
-          $_SESSION['login'] = $username; // Initializing Session
+    } else {
+      include 'connect.php';
+      // Define $username and $password
+      $username = $_POST['login'];
+      $password = md5($_POST['password']);
+      // SQL query to fetch information of registerd users and finds user match.
+      $query = "SELECT login, password FROM users WHERE login=? AND password=? LIMIT 1";
+      // To protect MySQL injection for Security purpose
+      $stmt = $conn->prepare($query);
+      $stmt->bind_param("ss", $username, $password);
+      $stmt->execute();
+      $stmt->bind_result($username, $password);
+      $stmt->store_result();
+      if($stmt->fetch()) { //fetching the contents of the row 
+        $_SESSION['login'] = $username; // Initializing Session
       }
       mysqli_close($conn); // Closing Connection
     }
+      
   }
-  public function SessionVerify(){
+  public function SessionVerify() {
     if(isset($_SESSION['login'])){
     header("location: includes/checkuser.php"); // Check user session and role
     }
   }
-  public function SessionCheck(){
+  public function SessionCheck() {
     global $conn;
     session_start();// Starting Session
     // Storing Session
@@ -44,7 +45,7 @@ class Login{
     $_SESSION["name"] = $row["name"];
     $_SESSION["role"] = $row["role"];
   }
-  public function UserType(){
+  public function UserType() {
     //if user role is 1, redirect to admin page
     if ($_SESSION["role"] == 1) {
       header("Location:../user/admin/");
@@ -55,10 +56,10 @@ class Login{
     }
   }
 }
+
 class UserFunctions{
-  public function UserName(){
+  public function UserName() {
     $username = $_SESSION["name"];
     echo $username;
   }
 }
-?>
